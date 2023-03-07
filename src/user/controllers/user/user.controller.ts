ac852073449223ef9df8360eed16be7261374b8d
Manish Thulung <manish.thulung58@gmail.com>
2023-03-07 23:01:46 +0545

    crud

--- src/user/controllers/user/user.controller.ts
+++ src/user/controllers/user/user.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateUser } from 'src/user/dtos/CreateUser.dto';
+import { UpdateUser } from 'src/user/dtos/UpdateUser.dto';
 import { UserService } from 'src/user/services/user/user.service';
 
 @Controller('user')
@@ -14,5 +23,15 @@ export class UserController {
   @Post()
   createUser(@Body() userDetails: CreateUser) {
     return this.userServices.createUser(userDetails);
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: number, @Body() userDetails: UpdateUser) {
+    return this.userServices.updateUser(id, userDetails);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: number) {
+    return this.userServices.deleteUser(id);
   }
 }
--- src/user/services/user/user.service.ts
+++ src/user/services/user/user.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, HttpStatus } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from 'src/entities/User';
-import { CreateUserParams } from 'src/utils/type';
+import { CreateUserParams, UpdateUserParams } from 'src/utils/type';
 
 @Injectable()
 export class UserService {
@@ -17,5 +17,27 @@ export class UserService {
 
   createUser(userDetailsParams: CreateUserParams) {
     return this.userRepository.save(userDetailsParams);
+  }
+
+  async updateUser(id: number, userDetailsParams: UpdateUserParams) {
+    const user = await this.userRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+    if (!user)
+      throw new HttpException('User Not Found!', HttpStatus.BAD_REQUEST);
+    return this.userRepository.update(id, userDetailsParams);
+  }
+
+  async deleteUser(id: number) {
+    const user = await this.userRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+    if (!user)
+      throw new HttpException('User Not Found!', HttpStatus.BAD_REQUEST);
+    return this.userRepository.delete(id);
   }
 }
